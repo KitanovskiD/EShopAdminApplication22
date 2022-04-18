ea077be4da95e1690585617db4a5e689e32bc58c
KitanovskiD <dimitar.kitanovski@outlook.com>
2022-04-18 16:05:29 +0200

    InitialCommit

---
+++ EShopAdminApplication/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using EShopAdminApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EShopAdminApplication.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+
+            string URL = "https://localhost:44386/api/Admin/GetOrders";
+
+            HttpResponseMessage response = client.GetAsync(URL).Result;
+
+            var result = response.Content.ReadAsAsync<List<Order>>().Result;
+
+            return View(result);
+        }
+    }
+}
